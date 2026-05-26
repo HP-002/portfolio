c47b5b575c5f06922bb77003f1a7faa5d43f89ec
Het Patel <hetfaldu19@gmail.com>
2026-05-26 16:01:04 -0400

    Update: Divide framworks and libraries according to types. Add couple new

--- src/assets/data/framesAndLibs.js
+++ src/assets/data/framesAndLibs.js
@@ -1,122 +1,130 @@
 import ReactIcon from "../images/react.svg"
 import ExpressIcon from "../images/express.svg"
-import ViteIcon from "../images//vite.svg"
+import ViteIcon from "../images/vite.svg"
 import NumPyIcon from "../images/numpy.svg"
 import PandasIcon from "../images/pandas.svg"
 import MatplotlibIcon from "../images/matplotlib.svg"
 import TensorFlowIcon from "../images/tensorflow.svg"
 import ScikitLearnIcon from "../images/scikitlearn.svg"
-import JUnitIcon from "../images/JUnit.svg"
+import JUnitIcon from "../images/junit.svg"
 import TailwindIcon from "../images/tailwind.svg"
 import MicroPyIcon from "../images/micropython.svg"
 import IpoptIcon from "../images/coinor.png"
 import ReactNativeIcon from "../images/react-native.svg"
+import PyTorchIcon from "../images/pytorch.svg"
+import OpenCVIcon from "../images/opencv.svg"
+import GNUBisonIcon from "../images/gnu-bison.png"
+import FastAPIIcon from "../images/fastapi.svg"
 
-const framesAndLibs = [
+const machineLearningFL = [
     {
-        id: "react-native",
-        name: 'React Native',
-        icon: ReactNativeIcon,
-        description: "Mobile Development",
-        insetColor: "white",
-        shadowColor: "cyan-500",
-    },
-    {
-        id: "react",
-        name: 'React',
-        icon: ReactIcon,
-        description: "Frontend Library",
-        insetColor: "white",
-        shadowColor: "cyan-500",
-    },
-    {
-        id: "express",
-        name: 'Express.js',
-        icon: ExpressIcon,
-        description: "Web App Framework",
-        insetColor: "white",
-        shadowColor: "white",
-    },
-    {
-        id: "vite",
-        name: 'Vite',
-        icon: ViteIcon,
-        description: "Build Tool",
-        insetColor: "yellow-400",
-        shadowColor: "violet-400",
-    },
-    {
-        id: "numpy",
-        name: 'NumPy',
-        icon: NumPyIcon,
-        description: "Python Library",
-        insetColor: "cyan-500",
-        shadowColor: "blue-500",
-    },
-    {
-        id: "pandas",
-        name: 'Pandas',
-        icon: PandasIcon,
-        description: "Data Analysis Library",
-        insetColor: "yellow-300",
-        shadowColor: "pink-500",
-    },
-    {
-        id: "matplotlib",
-        name: 'Matplotlib',
-        icon: MatplotlibIcon,
-        description: "Build Tool",
-        insetColor: "yellow-400",
-        shadowColor: "green-500",
+        id: "pytorch",
+        name: "PyTorch",
+        icon: PyTorchIcon,
+        description: "Deep Learning Framework",
     },
     {
         id: "tensorflow",
-        name: 'TensorFlow',
+        name: "TensorFlow",
         icon: TensorFlowIcon,
         description: "ML Library",
-        insetColor: "orange-500",
-        shadowColor: "orange-500",
     },
     {
         id: "scikitlearn",
-        name: 'scikit-learn',
+        name: "scikit-learn",
         icon: ScikitLearnIcon,
         description: "ML Library",
-        insetColor: "orange-500",
-        shadowColor: "sky-600",
     },
     {
-        id: "junit",
-        name: 'JUnit',
-        icon: JUnitIcon,
-        description: "Testing Framework",
-        insetColor: "red-400",
-        shadowColor: "green-500",
+        id: "opencv",
+        name: "OpenCV",
+        icon: OpenCVIcon,
+        description: "Computer Vision Library",
     },
     {
-        id: "tailwind",
-        name: 'Tailwind CSS',
-        icon: TailwindIcon,
-        description: "CSS Framework",
-        insetColor: "cyan-500",
-        shadowColor: "cyan-500",
+        id: "numpy",
+        name: "NumPy",
+        icon: NumPyIcon,
+        description: "Numerical Computing Library",
     },
     {
-        id: "micropython",
-        name: 'MicroPython',
-        icon: MicroPyIcon,
-        description: "Python for Microcontrollers",
-        insetColor: "white",
-        shadowColor: "white",
+        id: "pandas",
+        name: "Pandas",
+        icon: PandasIcon,
+        description: "Data Analysis Library",
     },
     {
-        id: "ipopt",
-        name: 'IPOPT',
-        icon: IpoptIcon,
-        description: "Nonlinear Optimization",
-        insetColor: "yellow-500",
-        shadowColor: "yellow-500",
+        id: "matplotlib",
+        name: "Matplotlib",
+        icon: MatplotlibIcon,
+        description: "Data Visualization Library",
     },
 ]
 
-export default framesAndLibs
+const webDevFL = [
+    {
+        id: "react-native",
+        name: "React Native",
+        icon: ReactNativeIcon,
+        description: "Mobile Development",
+    },
+    {
+        id: "react",
+        name: "React",
+        icon: ReactIcon,
+        description: "Frontend Framework",
+    },
+    {
+        id: "fastapi",
+        name: "FastAPI",
+        icon: FastAPIIcon,
+        description: "API Framework",
+    },
+    {
+        id: "express",
+        name: "Express.js",
+        icon: ExpressIcon,
+        description: "Backend Framework",
+    },
+    {
+        id: "vite",
+        name: "Vite",
+        icon: ViteIcon,
+        description: "Build Tool",
+    },
+    {
+        id: "tailwind",
+        name: "Tailwind CSS",
+        icon: TailwindIcon,
+        description: "CSS Framework",
+    },
+]
+
+const otherFL = [
+    {
+        id: "gnu-bison",
+        name: "GNU Bison",
+        icon: GNUBisonIcon,
+        description: "Parser for Compilers",
+    },
+    {
+        id: "junit",
+        name: "JUnit",
+        icon: JUnitIcon,
+        description: "Testing Framework",
+    },
+    {
+        id: "micropython",
+        name: "MicroPython",
+        icon: MicroPyIcon,
+        description: "Python for Microcontrollers",
+    },
+    {
+        id: "ipopt",
+        name: "IPOPT",
+        icon: IpoptIcon,
+        description: "Nonlinear Optimization",
+    },
+]
+
+export { machineLearningFL, webDevFL, otherFL }
