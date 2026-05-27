96cb32acf4aafd1683d3a2018e145fbb7f0e1b84
Het Patel <hetfaldu19@gmail.com>
2026-05-27 14:23:33 +0000

    Add: Contact information

---
+++ src/assets/data/contact.js
@@ -0,0 +1,14 @@
+
+const GITHUBID = "HP-002";
+const LINKEDINID = "hetpatel19";
+const PERSONALEMAIL = "hetfaldu19";
+const SCHOOLEMAIL = "hetpatel"
+
+const CONTACT = {
+    github: `https://github.com/${GITHUBID}`,
+    linkedin: `https://www.linkedin.com/in/${LINKEDINID}`,
+    personalEmail: `mailto:${PERSONALEMAIL}@gmail.com`,
+    schoolEmail: `mailto:${SCHOOLEMAIL}@buffalo.edu`,
+}
+
+export default CONTACT;
