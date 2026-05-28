d5640d289ff31453255434552fe13b839a29f3ee
Het Patel <hetfaldu19@gmail.com>
2026-05-28 00:55:03 +0000

    Add: Contact information

--- src/assets/data/contact.js
+++ src/assets/data/contact.js
@@ -4,11 +4,11 @@ const LINKEDINID = "hetpatel19";
 const PERSONALEMAIL = "hetfaldu19";
 const SCHOOLEMAIL = "hetpatel"
 
-const CONTACT = {
+const contact = {
     github: `https://github.com/${GITHUBID}`,
     linkedin: `https://www.linkedin.com/in/${LINKEDINID}`,
     personalEmail: `mailto:${PERSONALEMAIL}@gmail.com`,
     schoolEmail: `mailto:${SCHOOLEMAIL}@buffalo.edu`,
 }
 
-export default CONTACT;
+export default contact;
